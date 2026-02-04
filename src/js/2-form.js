e6936bff2508e9ba392eedc522e300b098c7f5fb
Viktoriia Tkachenko <vikustkachenko@gmail.com>
2026-02-04 15:38:05 +0100

    edit js

---
+++ src/js/2-form.js
@@ -0,0 +1,65 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  try {
+    const parsedData = JSON.parse(savedData);
+
+    formData = {
+      email: (parsedData.email ?? '').trim(),
+      message: (parsedData.message ?? '').trim(),
+    };
+
+    formEl.elements.email.value = formData.email;
+    formEl.elements.message.value = formData.message;
+  } catch (error) {
+ 
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+
+formEl.addEventListener('input', onFormInput);
+
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  const { name, value } = event.target;
+
+
+  if (name !== 'email' && name !== 'message') return;
+
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = formData.email.trim();
+  const message = formData.message.trim();
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log({ email, message });
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = { email: '', message: '' };
+  formEl.reset();
+}
